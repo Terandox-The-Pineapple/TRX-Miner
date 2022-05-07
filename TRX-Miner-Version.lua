ac32eb278a1fb238134dec78d37bf5f2a5138525
Terandox <rene-gilbert99@web.de>
2022-05-07 20:27:16 +0200

    Updated version to 1.25
    Changed version number to 1.25

--- TRX-Miner-Version.lua
+++ TRX-Miner-Version.lua
@@ -1,6 +1,6 @@
 local data = require("data")
 
-local version = 1.24
+local version = 1.25
 
 if fs.exists("trx-miner-version") then data.set("version",version,"trx-miner-version-check")
 elseif fs.exists("trx-miner.version") then data.set("version",version,"trx-miner.cversion") end
